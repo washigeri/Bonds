b86717701d6daa8d63ab1c9ceeeb84271dd9de1d
Washigeri <malek.elouerghi@gmail.com>
2017-12-19 02:33:52 +0100

    Suppresion scène test_options, ajout GameOver (reste à le relier au reste du jeu)

---
+++ Assets/Scripts/UI/GameOverScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScript : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+        GameManager gm = GameManager.gameManager;
+        Button mainMenu = transform.Find("MenuPrincipal").GetComponent<Button>();
+        Button quit = transform.Find("Quitter").GetComponent<Button>();
+        mainMenu.onClick.AddListener(delegate
+        {
+            gm.GoBackToMenu();
+        });
+        quit.onClick.AddListener(delegate
+        {
+            gm.Quit();
+        });
+    }
+}
